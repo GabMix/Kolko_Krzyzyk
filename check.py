06679ca9c49faa576dcea1ba30e4800fc5896b31
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2024-05-04 15:03:56 +0200

    Create check

---
+++ check.py
@@ -0,0 +1,16 @@
+def sym():
+    print("Choose your options: ")
+    symbol1 = input("Player 1: X or O? ")
+    if symbol1 == "X":
+        symbol2 = "O"
+        print("Player 1 : X")
+        print("Player 2 : O ")
+    else:
+        symbol2 = "X"
+        print("Player 1 : O ")
+        print("Player 2: X. ")
+    print("\n")
+    return symbol1, symbol2
+
+
+symbol1, symbol2 = sym()
