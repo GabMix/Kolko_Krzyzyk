ee10d443dbbec9d046b371ec7577c0795018c583
Piotr Mlak <mlak.piotr2@gmail.com>
2024-05-04 22:32:21 +0200

    adding another validation to move

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ class Game:
         self.board = []
         self.boardSize = boardSize
         self.activePlayer = 1
-        self.isFinished = False
+        self.state = 0  # 0 - in progress; 1 - X won, 2 - Y won, -1 - tie
 
         for i in range(boardSize):
             self.board.append([])
@@ -13,5 +13,20 @@ class Game:
     def makeMove(self, x, y):
         if x >= self.boardSize or y >= self.boardSize:
             return
+        
+        if self.board[x][y] != 0:
+            return
+
+        if self.state != 0:
+            return
 
         self.board[x][y] = self.activePlayer
+
+        self.checkIfGameOver()
+        #TODO: zmienić gracza
+
+    #TODO: sprawidzić czy jest remis, opowiednio zmienić stan gry (self.state), należy sprawdzić kolumny, wiersze i proste, uwzględnić rozmiary planszy
+    #TODO: sprawdzić czy któryś gracz wygrał, zmienić stan gry
+    def checkIfGameOver(self):
+        print("")
+
