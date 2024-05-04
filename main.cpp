868cebd17b820c28753c753b4bdcfaf3a09e0ef9
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2024-05-04 14:56:30 +0200

    zmiana z cpp na python

--- main.cpp
+++ main.cpp
@@ -1,28 +1,12 @@
-#include <iostream>
+def draw_board(board):
+    print("-------------")
+    for i in range(len(board)):
+        print("| ", end="")
+        for j in range(len(board[i])):
+            print(board[i][j], "| ", end="")
+        print("\n-------------")
 
-using namespace std;
-
-#define COLS 3
-#define ROWS 3
-
-void drawBoard(char board[COLS][ROWS]) 
-{ 
-    cout << "-------------\n"; 
-    for (int i = 0; i < COLS; i++) 
-    { 
-        cout << "| "; 
-        for (int j = 0; j < ROWS; j++) 
-        { 
-            cout << board[i][j] << " | "; 
-        } 
-        cout << "\n-------------\n"; 
-    } 
-} 
-
-int main()
-{
-    char board[COLS][ROWS] = { { ' ', ' ', ' ' }, 
-                         { ' ', ' ', ' ' }, 
-                         { ' ', ' ', ' ' } }; 
-    drawBoard(board);
-}
+rows = 3
+cols = 3
+board = [[' ' for _ in range(cols)] for _ in range(rows)]
+draw_board(board)
