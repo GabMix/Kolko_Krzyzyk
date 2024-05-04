8b216bee57e231349b21881f36dc38fb24161cab
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2024-05-04 14:30:18 +0200

    Tablica

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "raylib.h"
+#include "raymath.h"
+
+using namespace std;
+
+#define COLS 3
+#define ROWS 3
+
+void drawBoard(char board[COLS][ROWS]) 
+{ 
+    cout << "-------------\n"; 
+    for (int i = 0; i < COLS; i++) 
+    { 
+        cout << "| "; 
+        for (int j = 0; j < ROWS; j++) 
+        { 
+            cout << board[i][j] << " | "; 
+        } 
+        cout << "\n-------------\n"; 
+    } 
+} 
+
+int main()
+{
+    char board[COLS][ROWS] = { { ' ', ' ', ' ' }, 
+                         { ' ', ' ', ' ' }, 
+                         { ' ', ' ', ' ' } }; 
+    drawBoard(board);
+}
