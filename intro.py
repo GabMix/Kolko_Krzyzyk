c8ef0f5b6616beb4c2ef771e0f52e2650070f85c
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2024-05-09 02:11:18 +0200

    intro

---
+++ intro.py
@@ -0,0 +1,7 @@
+def description():
+    print("Witaj w grze Kolko i Krzyzyk!")
+    print("\n")
+    print("Zasady: Gracz 1 i Gracz 2 zyskuja unikatowe symbole - 'X' i 'O'. Na wyswietlonej planszy wymiaru 3x3, 4x4 lub 5x5, "
+         "gracze zaznaczaja wymiennie swoj symbol. Wygrywa ten, ktoremu uda sie ustawic O/X diagonalnie, w pionowej, badz poziomej linii tak, aby wystapily one pod rzad.")
+    print("\n")
+    
