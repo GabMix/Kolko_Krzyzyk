44c75f726fbbe1195a046378daf53bda39bb90d5
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2024-05-16 02:06:00 +0200

    Update and rename openPNG to openPNG.py
    gdyby ktos chcial otworzyc z poziomu github - sciezka jest dodana moja, poniewaz konwertowalam zdjecie na png

--- openPNG.py
+++ openPNG.py
@@ -7,6 +7,8 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 image_path = "C:\\Users\\dagak\\OneDrive\\Pulpit\\1a778b59-a942-4350-8e97-f5b646f00e45.png"
+#sciezka githubowa!!:
+#https://github.com/GabMix/Kolko_Krzyzyk/blob/1-koncept-graficzny-interface/1a778b59-a942-4350-8e97-f5b646f00e45.jpg
 photo = tk.PhotoImage(file=image_path)
 label = tk.Label(frame, image=photo)
 label.pack()
