9768d4225719d5b98b0b1e328c4ac1bcb9108430
smleko <sebastianmleko02@gmail.com>
2024-06-04 13:44:39 +0200

    Gotowe MainMenu

---
+++ Gamemode_Menu.py
@@ -0,0 +1,40 @@
+from Game_Board import *
+
+class Choose:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("768x768")
+        self.root.title("Wybór trybu gry")
+
+        # Tymczasowy design ------------------------------------
+        boardsize = 3
+        # Tworzenie siatki
+        for i in range(boardsize):
+            self.root.rowconfigure(i, weight=1)
+            self.root.columnconfigure(i, weight=1)
+
+        # Tworzenie przyciskow
+        self.btn = [[tk.Button(self.root, text="Tymczasowy design") for i in range(boardsize)] for j in range(boardsize)]
+        # Przypisywanie przyciskow do siatki
+        for x in range(boardsize):
+            for y in range(boardsize):
+                self.btn[x][y].grid(row=y, column=x, sticky="news")  # news = north east west south
+        self.btn[0][0].configure(text="3x3", command=self.run_3x3)
+        self.btn[1][0].configure(text="4x4", command=self.run_4x4)
+        self.btn[2][0].configure(text="5x5", command=self.run_5x5)
+
+        # koniec tymczasowego designu ------------------------------
+
+        self.root.mainloop()
+
+    def run_3x3(self):
+        self.root.destroy()
+        Game(3)
+
+    def run_4x4(self):
+        self.root.destroy()
+        Game(4)
+
+    def run_5x5(self):
+        self.root.destroy()
+        Game(5)
---
+++ Main_Menu.py
@@ -0,0 +1,43 @@
+from Gamemode_Menu import *
+
+class MainMenu:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("1024x1024")
+        self.root.title("Kółko i Krzyżyk")
+        self.root.resizable(False, False)
+        # Tlo + Ramka
+        self.background_image = tk.PhotoImage(file="mainmenu.png")
+        self.background = tk.Label(self.root, image=self.background_image)
+        self.background.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # Start
+        self.btn_start_image = tk.PhotoImage(file="start_btn.png")
+        self.btn_start = tk.Button(self.root, image=self.btn_start_image, borderwidth=0, activebackground='black', command=self.run_choose_game)
+        self.btn_start.place(x=512-200, y=330, width=398, height=115)
+
+        # Statystyki
+        self.btn_statystyki_image = tk.PhotoImage(file="statystyki_btn.png")
+        self.btn_statystyki = tk.Button(self.root, image=self.btn_statystyki_image, borderwidth=0, activebackground='black', command=self.run_statystyki)
+        self.btn_statystyki.place(x=512-200, y=330+150, width=398, height=115)
+
+        # Twórcy
+        self.btn_tworcy_image = tk.PhotoImage(file="tworcy_btn.png")
+        self.btn_tworcy = tk.Button(self.root, image=self.btn_tworcy_image, borderwidth=0, activebackground='black', command=self.run_tworcy)
+        self.btn_tworcy.place(x=512-200, y=330+300, width=398, height=115)
+
+
+        self.root.mainloop()
+        self.root.quit()
+
+    def run_choose_game(self):
+        Choose()
+
+    def run_statystyki(self):
+        print("ERROR: Statystyki nie gotowe")
+
+    def run_tworcy(self):
+        print("ERROR: Twórcy nie gotowe")
+
+
+MainMenu()
