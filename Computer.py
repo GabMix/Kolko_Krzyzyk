9b4f187d72d6061ec2ac5c69acfe2199876090b9
krzysztofmazurkiewicz <164171816+krzysztofmazurkiewicz@users.noreply.github.com>
2024-06-08 17:02:31 +0200

    Minimax algorithm choosing best move

--- Computer.py
+++ Computer.py
@@ -18,3 +18,41 @@ class Computer:
         else:
             self.game.endGame()
     
+    def minimax(self, depth, computer_turn : bool):
+        self.game.checkIfGameOver()
+        if self.game.state == -1:
+            return 0
+        elif self.game.state == 1:
+            return 1
+        elif self.game.state == 2:
+            return -1
+
+        if computer_turn == 1: #Sprawdzanie maksymalnego wyniku dla komputera
+            best_score = -2
+            for y in range(self.game.boardSize):
+                for x in range(self.game.boardSize):
+                    if self.game.board[y][x] == 0:
+                        self.game.board[y][x] = self.game.activePlayer
+                    score = self.minimax( depth + 1, False)
+                    self.game.board[y][x] = 0
+                    best_score = max(score, best_score)
+            return best_score
+
+        elif computer_turn == 1: #Sprawdzanie minimalneg wyniku dla gracza
+            best_score = -2
+            for y in range(self.game.boardSize):
+                for x in range(self.game.boardSize):
+                    if self.game.board[y][x] == 0:
+                        self.game.switchPlayer()
+                        self.game.board[y][x] = self.game.activePlayer
+                        self.game.switchPlayer()
+                    score = self.minimax( depth + 1, True)
+                    self.game.board[y][x] = 0
+                    best_score = min(score, best_score)
+            return best_score
+
+        self.game.checkIfGameOver()
+        if self.game.state == 0:
+            self.game.switchPlayer()
+        else:
+            self.game.endGame()
