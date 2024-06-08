44518da8ec2593d3a57a60afb068b8b5ae3de22d
krzysztofmazurkiewicz <164171816+krzysztofmazurkiewicz@users.noreply.github.com>
2024-06-08 17:01:46 +0200

    Computer move easy mode

--- Computer.py
+++ Computer.py
@@ -7,4 +7,14 @@ class Computer:
         self.difficulty = difficulty_lvl
         self.game = Game(BoardSize)
 
+    def easy_comp_move(self): #Ruch komputera w trybie początkującym
+        #self.game.makeMove(randrange(self.game.boardSize), randrange(self.game.boardSize))
+        if self.game.board[randrange(self.game.boardSize)][randrange(self.game.boardSize)] != 0:
+            self.game.board[randrange(self.game.boardSize)][randrange(self.game.boardSize)] = self.game.activePlayer
+
+        self.game.checkIfGameOver()
+        if self.game.state == 0:
+            self.game.switchPlayer()
+        else:
+            self.game.endGame()
     
