25fcd9721fd029c9660b9acc86eb5005c7711d04
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2024-06-11 18:30:19 +0200

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,30 +1,55 @@
+import tkinter as tk
 import json
 import os
+    def run_statystyki(self):
+        self.show_stats()
 
+    def run_tworcy(self):
+        print("ERROR: Twórcy nie gotowe")
 
-#updatujemy stats wygranych remisow i przegranych
-def update_stats(self, result):
-    stats = self.load_stats()
-    if result ==-1:  
-        stats["Draws"]+=1
-    else:
-        stats["Wins"][self.players[result]] = stats["Wins"].get(self.players[result], 0) + 1
-        loser = 2 if result == 1 else 1
-        stats["Losses"][self.players[loser]] =stats["Losses"].get(self.players[loser],0)+1
-    self.save_stats(stats)
+    def load_stats(self):
+        if os.path.exists("stats.json"):
+            with open("stats.json", "r") as file:
+                return json.load(file)
+        return {"Wins": {"Gracz 1": 0, "Gracz 2": 0}, "Losses": {"Gracz 1": 0, "Gracz 2": 0}, "Draws": 0}
 
+    def save_stats(self, stats):
+        with open("stats.json", "w") as file:
+            json.dump(stats, file)
 
-#zapisywanie statystyk
-def load_stats(self):
-    if os.path.exists("stats.json"):
-        with open("stats.json", "r") as file:
-            return json.load(file)
-    return {"Wins": {"Player 1": 0, "Player 2": 0}, "Losses": {"Player 1": 0, "Player 2": 0}, "Draws": 0}
+    def show_stats(self):
+        stats = self.load_stats()
+        stats_window = tk.Toplevel(self.root)
+        stats_window.geometry("400x300")
+        stats_window.title("Statystyki")
 
+        wins_label = tk.Label(stats_window,
+                              text=f"Wygrane:\nPlayer 1: {stats['Wins']['Player 1']}\nPlayer 2: {stats['Wins']['Player 2']}")
+        wins_label.pack()
 
-def save_stats(self, stats):
-    with open("stats.json", "w") as file:
-        json.dump(stats, file)
+        losses_label = tk.Label(stats_window,
+                                text=f"Przegrane:\nPlayer 1: {stats['Losses']['Player 1']}\nPlayer 2: {stats['Losses']['Player 2']}")
+        losses_label.pack()
 
+        draws_label = tk.Label(stats_window, text=f"Remisy: {stats['Draws']}")
+        draws_label.pack()
+
+        back_button = tk.Button(stats_window, text="Powrót do menu", command=self.back_to_main_menu)
+        back_button.pack()
+
+    def update_stats(self, result):
+        stats = self.load_stats()
+        if result == -1:
+            stats["Draws"] += 1
+        else:
+            winner = "Player 1" if result == 1 else "Player 2"
+            loser = "Player 2" if result == 1 else "Player 1"
+            stats["Wins"][winner] += 1
+            stats["Losses"][loser] += 1
+        self.save_stats(stats)
+
+    def back_to_main_menu(self):
+        self.root.destroy()
+        self.__init__()
 
 
